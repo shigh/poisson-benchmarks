6ba30f84ca2ecb03fe48e5ffd02c34ff21e72922
shigh <highscot@gmail.com>
2014-12-14 20:29:42 -0600

    Added bad link arg.
    This is bad practice, but is the only way I can get it to run on taub.

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,7 @@ import numpy as np
 
 mpi_compile_args = os.popen("mpic++ --showme:compile").read().strip().split(' ')
 mpi_link_args    = os.popen("mpic++ --showme:link").read().strip().split(' ')
+link_args = ["-L/home/shigh2/src/fftw/lib", "-lfftw3", "-lfftw3_mpi", "-lm"]
 
 setup(
     cmdclass = {'build_ext':build_ext},
@@ -14,8 +15,7 @@ setup(
                              ["testsolvers.pyx"],
                              extra_objects=["fftwlocal.o", "problem.o", "mpi_stats.o",
                                             "fftwmpi.o"],
-                             libraries=["fftw3", "fftw3_mpi", "m"],
                              extra_compile_args=mpi_compile_args,
-                             extra_link_args=mpi_link_args,
+                             extra_link_args=link_args+mpi_link_args,
                              language="c++"),]
     )
